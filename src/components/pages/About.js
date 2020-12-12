954e8ba79f1ccefa773320b238aaa8528287e6db
Myroslav <andreykiv09@gmail.com>
2020-12-12 14:18:11 +0100

    init commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a TodoList app v.1.0.0. It is part of a <a href='https://www.youtube.com/watch?v=sBws8MSXN7A&ab_channel=TraversyMedia'>React crash course.</a></p>
+        </React.Fragment>
+    )
+}
+
+export default About
